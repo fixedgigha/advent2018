153e5f21f9467f9a16cee5ac946c6de9b20baf5f
James Speakman <james.speakman@bjss.com>
2018-12-04 17:35:12 +0000

    Eight

--- src/main/kotlin/advent2018/day4/Day4.kt
+++ src/main/kotlin/advent2018/day4/Day4.kt
@@ -10,6 +10,7 @@ val guardRegex = Regex("Guard #(\\d+) begins shift")
 val timeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
 
 data class Sleep(val sleep: LocalDateTime, var wake: LocalDateTime? = null)
+
 class GuardSummary(val guard: Int, events: List<Sleep>) {
     val totalSleep: Long
     val mostFrequentMinute: Map.Entry<Int, Int>
@@ -31,34 +32,36 @@ class GuardSummary(val guard: Int, events: List<Sleep>) {
     }
 }
 
+fun linesToSleepMap(lines : List<String>): Map<Int, List<Sleep>> =
+    lines
+        .sorted()
+        .fold(Pair(mutableMapOf<Int, MutableList<Sleep>>(), 0)) { (events, currentGuardInt), line ->
+            val lineMatch = inputRegex.matchEntire(line)
+            var nextGuardInt = currentGuardInt
+            if (lineMatch != null) {
+                val guardMatch = guardRegex.matchEntire(lineMatch.groupValues[2])
+                if (guardMatch != null) {
+                    nextGuardInt = guardMatch.groupValues[1].toInt()
+                }
+                else {
+                    val time = LocalDateTime.parse(lineMatch.groupValues[1], timeFormat)
+                    val guardEvents = events.getOrDefault(currentGuardInt, mutableListOf())
+                    if (lineMatch.groupValues[2] == "falls asleep") {
+                        guardEvents.add(Sleep(time))
+
+                    }
+                    else {
+                        guardEvents.last().wake = time
+                    }
+                    events[currentGuardInt] = guardEvents
+                }
+            }
+            Pair(events, nextGuardInt)
+        }.first
+
 fun main(args: Array<String>) {
     val lines = File("src/main/resources/day4/input.txt").readLines()
-    val sorted = lines.sorted()
-    var currentGuardInt = 0
-    var initialMap = mutableMapOf<Int, MutableList<Sleep>>()
-    val finalMap =
-        sorted.fold(initialMap) { events, line ->
-           val lineMatch = inputRegex.matchEntire(line)
-           if (lineMatch != null) {
-               val time = LocalDateTime.parse(lineMatch.groupValues[1], timeFormat)
-               val guardMatch = guardRegex.matchEntire(lineMatch.groupValues[2])
-               if (guardMatch != null) {
-                   currentGuardInt = guardMatch.groupValues[1].toInt()
-               }
-               else {
-                   val guardEvents = events.getOrDefault(currentGuardInt, mutableListOf())
-                   if (lineMatch.groupValues[2] == "falls asleep") {
-                       guardEvents.add(Sleep(time))
-
-                   }
-                   else {
-                       guardEvents.last().wake = time
-                   }
-                   events[currentGuardInt] = guardEvents
-               }
-           }
-           events
-    }
+    val finalMap = linesToSleepMap(lines)
     val summaries = finalMap.map { it.key to GuardSummary(it.key, it.value) }.toMap()
     val winner = summaries.values.sortedByDescending { it.totalSleep } .first()
     val bestMinute = winner.mostFrequentMinute.key
@@ -67,5 +70,6 @@ fun main(args: Array<String>) {
 
     val highestFreq = summaries.values.sortedByDescending { it.mostFrequentMinute.value }
     val freqWinner = highestFreq.first()
+
     println(freqWinner.guard * (freqWinner.mostFrequentMinute.key))
 }
