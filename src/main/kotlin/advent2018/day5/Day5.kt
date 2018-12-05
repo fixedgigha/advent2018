15a89f23439521dc2706b99fa03b69ed6f030ed3
James Speakman <james.speakman@bjss.com>
2018-12-05 09:59:07 +0000

    Nine

---
+++ src/main/kotlin/advent2018/day5/Day5.kt
@@ -0,0 +1,40 @@
+package advent2018.day5
+
+import java.io.File
+
+fun swapCase(c : Char): Char = if (c.isUpperCase()) c.toLowerCase() else c.toUpperCase()
+
+val NUL = 0.toChar()
+
+fun react(input : String): Int {
+    var text = input.toCharArray()
+    var length = 0
+    while (length == 0) {
+        for (x in 0..(text.size - 2)) {
+            if (text[x] == swapCase(text[x + 1])) {
+                text[x] = NUL
+                text[x + 1] = NUL
+            }
+        }
+        val newText = text.filter { it != NUL }.fold(StringBuilder()) { sb, c ->
+            sb.append(c)
+        }.toString()
+        if (text.size == newText.length) {
+            length = newText.length
+        }
+        else {
+            text = newText.toCharArray()
+        }
+    }
+    return length
+}
+
+fun main(args: Array<String>) {
+    var text = File("src/main/resources/day5/input.txt").readText()
+    val analysis = mutableMapOf<Char, Int>()
+    for (a in 'a' .. 'z') {
+        val score = react(text.replace("" + a, "", true))
+        analysis[a] = score
+    }
+    println(analysis.entries.sortedBy { it.value }.first())
+}
