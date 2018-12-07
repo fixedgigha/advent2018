21f7f512e659cf751acae62ecbc358fbb25a4e19
James Speakman <james.speakman@bjss.com>
2018-12-07 16:20:35 +0000

    Sixteen

--- src/main/kotlin/advent2018/day7/Day7_2.kt
+++ src/main/kotlin/advent2018/day7/Day7_2.kt
@@ -16,63 +16,67 @@ data class Candidate (val letter: Char, val immediatePredecessor: List<Candidate
 
 val NON_CANDIDATE = Candidate('-')
 
-fun main(args: Array<String>) {
-    val lineRegex = Regex("Step ([A-Z]) must be finished before step ([A-Z]) can begin\\.")
-    val (antecedents, subsequents, candidates) = File("src/main/resources/day7/input.txt")
-        .readLines()
-        .fold(
-            Triple(
-                mutableMapOf<Char, MutableList<Char>>(),
-                mutableMapOf<Char, MutableList<Char>>(),
-                mutableSetOf<Char>()
-            )
-        ) { (antecedents, subsequents, candidates), line ->
-            val match = lineRegex.matchEntire(line)
-            if (match != null) {
-                val antecedent = match.groupValues[1][0]
-                val subsequent = match.groupValues[2][0]
-                candidates.add(antecedent)
-                candidates.add(subsequent)
+fun loadData() = File("src/main/resources/day7/input.txt")
+    .readLines()
+    .fold(
+        Triple(
+            mutableMapOf<Char, MutableList<Char>>(),
+            mutableMapOf<Char, MutableList<Char>>(),
+            mutableSetOf<Char>()
+        )
+    ) { (antecedents, subsequents, candidates), line ->
+        val match = lineRegex.matchEntire(line)
+        if (match != null) {
+            val antecedent = match.groupValues[1][0]
+            val subsequent = match.groupValues[2][0]
+            candidates.add(antecedent)
+            candidates.add(subsequent)
 
-                antecedents[subsequent] = antecedents.getOrDefault(subsequent, mutableListOf())
-                antecedents[subsequent]?.add(antecedent)
-                subsequents[antecedent] = subsequents.getOrDefault(antecedent, mutableListOf())
-                subsequents[antecedent]?.add(subsequent)
-            }
-
-            Triple(antecedents, subsequents, candidates)
+            antecedents[subsequent] = antecedents.getOrDefault(subsequent, mutableListOf())
+            antecedents[subsequent]?.add(antecedent)
+            subsequents[antecedent] = subsequents.getOrDefault(antecedent, mutableListOf())
+            subsequents[antecedent]?.add(subsequent)
         }
-    var candidatesWithNoAntecedents = mutableListOf<Candidate>()
-    candidatesWithNoAntecedents.addAll(candidates.filter { !antecedents.contains(it) }.map { Candidate(it) })
-    println("No antecedents $candidatesWithNoAntecedents")
-    val threads = listOf(
-            mutableListOf<Candidate>(),
-            mutableListOf<Candidate>(),
-            mutableListOf<Candidate>(),
-            mutableListOf<Candidate>(),
-            mutableListOf<Candidate>()
-    )
+
+        Triple(antecedents, subsequents, candidates)
+    }
+
+val threads = listOf(
+    mutableListOf<Candidate>(),
+    mutableListOf<Candidate>(),
+    mutableListOf<Candidate>(),
+    mutableListOf<Candidate>(),
+    mutableListOf<Candidate>()
+)
+
+fun main(args: Array<String>) {
+    val (antecedents, subsequents, candidates) = loadData()
+    val currentWave = mutableListOf<Candidate>()
+    currentWave.addAll(candidates.filter { !antecedents.contains(it) }.map { Candidate(it) })
+    println("First wave $currentWave")
     val inPlay = mutableSetOf<Char>()
     var iteration = 0
-    while (candidatesWithNoAntecedents.isNotEmpty() ) {
-        inPlay.addAll(candidatesWithNoAntecedents.map {it.letter})
-        val nextCandidatesWithNoAntecedents = mutableListOf<Candidate>()
+    while (currentWave.isNotEmpty() ) {
+        inPlay.addAll(currentWave.map {it.letter})
+        val nextWave = mutableListOf<Candidate>()
 
-        for (x in 0..4) {
-            threads[x].add(iteration, if (candidatesWithNoAntecedents.size > x) candidatesWithNoAntecedents[x] else NON_CANDIDATE)
+        for (x in 0..threads.lastIndex) {
+            threads[x].add(iteration, if (currentWave.size > x) currentWave[x] else NON_CANDIDATE)
         }
-        for (candidatesWithNoAntecedent in candidatesWithNoAntecedents) {
-            val nextSubsequents = subsequents[candidatesWithNoAntecedent.letter]
+        for (candidate in currentWave) {
+            val nextSubsequents = subsequents[candidate.letter]
             if (nextSubsequents != null) {
-                val newCandidates = nextSubsequents.filter {
-                    (!inPlay.contains(it)) && inPlay.containsAll(antecedents[it] ?: mutableListOf())
-                }
-                nextCandidatesWithNoAntecedents.addAll(newCandidates.map {Candidate(it, listOf(candidatesWithNoAntecedent))})
+                nextWave.addAll(nextSubsequents
+                    .filter {
+                        inPlay.containsAll(antecedents[it] ?: mutableListOf())
+                    }
+                    .map {Candidate(it, listOf(candidate))}
+                )
             }
         }
-        candidatesWithNoAntecedents.clear()
-        val mergedNext = nextCandidatesWithNoAntecedents.groupBy { it.letter } .map {entry -> Candidate(entry.key, entry.value.flatMap{it.immediatePredecessor})}
-        candidatesWithNoAntecedents.addAll(mergedNext)
+        currentWave.clear()
+        val mergedNext = nextWave.groupBy { it.letter } .map { entry -> Candidate(entry.key, entry.value.flatMap{it.immediatePredecessor})}
+        currentWave.addAll(mergedNext)
         iteration++
     }
     println(threads[0].last())
