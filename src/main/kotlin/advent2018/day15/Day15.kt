7ce5f9594a5e2995e1a9a7218589bd002bd28b0f
fixedgigha <emissions@cowspassage.net>
2018-12-15 14:20:14 +0000

    Day15

--- src/main/kotlin/advent2018/day15/Day15.kt
+++ src/main/kotlin/advent2018/day15/Day15.kt
@@ -15,6 +15,16 @@ data class Dude(val type: Char, var x: Int, var y: Int, var health: Int = 200): 
 
 }
 
+class ComparablePoint(val point: Pair<Int, Int>) : Comparable<ComparablePoint> {
+    override fun compareTo(other: ComparablePoint): Int =
+        when {
+            point.second < other.point.second -> -1
+            point.second == other.point.second && point.first < other.point.first -> -1
+            point.second == other.point.second && point.first == other.point.first -> 0
+            else -> 1
+        }
+}
+
 class Game(fileName: String) {
 
     private val map: List<CharArray>
@@ -44,53 +54,82 @@ class Game(fileName: String) {
         return Pair(map, dudes)
     }
 
-    private fun attackable(dude: Dude, targets: List<Dude>): List<Dude> {
+    private fun attackableTargets(dude: Dude, targets: List<Dude>): List<Dude> {
         return targets.filter {
-            (it.x - dude.x).absoluteValue == 1 && (it.y - dude.y).absoluteValue == 1
+            ((it.x - dude.x).absoluteValue == 1 && it.y == dude.y) ||
+            ((it.y - dude.y).absoluteValue == 1 && it.x == dude.x)
         }
     }
 
-    private fun dudeInSpace(x: Int, y: Int, dudes: List<Dude>) =
+    private fun coordDude(x: Int, y: Int) =
+        dudes.find { it.alive() && it.x == x && it.y == y }
+
+    private fun coordIsDude(x: Int, y: Int) =
         dudes.find { it.alive() && it.x == x && it.y == y } != null
 
-    private fun mapIsSpace(x: Int, y: Int, map: List<CharArray>) =
+    private fun coordIsSpace(x: Int, y: Int) =
         map[y][x] == '.'
 
-    private fun openSpacesAround(x: Int, y: Int, map: List<CharArray>, dudes: List<Dude>) =
+    private fun openSpacesAround(x: Int, y: Int) =
         listOf(
             Pair(x - 1, y),
             Pair(x + 1, y),
             Pair(x, y - 1),
             Pair(x, y + 1)
         ).filter { (x, y) ->
-            mapIsSpace(x, y, map) && !dudeInSpace(x, y, dudes)
+            coordIsSpace(x, y) && !coordIsDude(x, y)
         }
 
 
-    private fun inRange(targets: List<Dude>, map: List<CharArray>, dudes: List<Dude>) =
+    private fun inRange(targets: List<Dude>) =
         targets.flatMap {
-            openSpacesAround(it.x, it.y, map, dudes)
+            openSpacesAround(it.x, it.y)
         }
 
     private fun routesToTarget(
         x: Int,
         y: Int,
         target: Pair<Int, Int>,
-        map: List<CharArray>,
-        dudes: List<Dude>,
         soFar: List<Pair<Int, Int>> = emptyList()
-    ): List<Pair<Int, Int>> {
-
-        return soFar
+    ): List<List<Pair<Int, Int>>> {
+        val newFar = mutableListOf<Pair<Int, Int>>()
+        newFar.addAll(soFar)
+        newFar.add(Pair(x, y))
+        if (x == target.first && y == target.second) {
+            return listOf(newFar)
+        }
+        return openSpacesAround(x, y).filter { !soFar.contains(it) }.flatMap { routesToTarget(it.first, it.second, target, newFar)}
     }
 
-    private fun takeARound(dude: Dude, map: List<CharArray>, dudes: List<Dude>): Boolean {
+    private fun takeARound(dude: Dude, dudes: List<Dude>): Boolean {
+        if (!dude.alive()) return false
         val targets = dudes.filter { it.alive() && it.type != dude.type }
         if (targets.isEmpty()) return true // game over
-        var attackable = attackable(dude, targets)
+        var attackable = attackableTargets(dude, targets)
         if (attackable.isEmpty()) {
-            val inRange = inRange(targets, map, dudes)
-            val routesToTarget = inRange.map { routesToTarget(dude.x, dude.y, it, map, dudes) }
+            // workout next move
+            val inRange = inRange(targets)
+            val routesToTarget = inRange
+                .flatMap { routesToTarget(dude.x, dude.y, it) }
+                .sortedBy { it.size }
+            if (routesToTarget.isNotEmpty()) {
+                val shortestRouteLen = routesToTarget.first().size
+                val nextMove =
+                    routesToTarget.takeWhile { it.size == shortestRouteLen }.sortedBy { ComparablePoint(it[1]) }.first()[1]
+                // move
+                dude.x = nextMove.first
+                dude.y = nextMove.second
+                attackable = attackableTargets(dude, targets)
+            }
+        }
+        if(attackable.isNotEmpty()) {
+            val lowestHealth = attackable.sortedBy { it.health }.first().health
+            val victim = attackable
+                .sortedBy { it.health }
+                .takeWhile { it.health == lowestHealth }
+                .sortedBy { ComparablePoint(Pair(it.x, it.y)) }
+                .first()
+            victim.health -= 3
         }
         return false
     }
@@ -98,18 +137,30 @@ class Game(fileName: String) {
     fun round(): Boolean {
         val liveDudes = liveDudes()
         return liveDudes.fold(false) {outcome, dude ->
-                outcome || takeARound(dude, map, liveDudes)
+                outcome || takeARound(dude, liveDudes)
         }
     }
 
     fun liveDudes() = dudes.filter{ it.health > 0 }.sorted()
+
+    fun drawBoard() {
+        map.forEachIndexed {y, row ->
+            row.forEachIndexed {x, cell ->
+                print(coordDude(x, y)?.type?:cell)
+            }
+            println()
+        }
+        println(liveDudes())
+    }
 }
 
 fun main(vararg args: String) {
     val game = Game("testInput.txt")
     var round = 0
-    while (round < 5) {
+    while (round < 50) {
         if (game.round()) break
+        println("After round $round")
+        game.drawBoard()
         round++
     }
     val result = game.liveDudes().map { it.health }.sum() * round
