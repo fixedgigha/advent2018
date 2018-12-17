6608800b90b4ebbccc024fe0f3f213732fcbc545
fixedgigha <emissions@cowspassage.net>
2018-12-17 21:14:40 +0000

    Day17

--- src/main/kotlin/advent2018/day17/Day17.kt
+++ src/main/kotlin/advent2018/day17/Day17.kt
@@ -1,14 +1,18 @@
 package advent2018.day17
 
+import advent2018.day10.pointsToGrid
 import java.io.File
 
 data class Clay(val x: IntRange, val y: IntRange)
 
 class Game(fileName: String) {
 
-    val map = loadData("testInput.txt")
+    val map = loadData(fileName)
     val maxY = map.fold(0) { highest, clay -> Math.max(highest, clay.y.last)}
-    val waterPoints = mutableSetOf(Pair(500, 0))
+    private val minX = map.fold(Int.MAX_VALUE) { lowest, clay -> Math.min(lowest, clay.x.last)}
+    private val maxX = map.fold(0) { highest, clay -> Math.max(highest, clay.x.last)}
+    val waterPoints = mutableSetOf<Pair<Int, Int>>()
+    val alltimeDanglers = mutableSetOf<Pair<Int, Int>>()
 
     private fun makeRange(input: String): IntRange =
         Regex("(\\d+)\\.\\.(\\d+)").matchEntire(input)?.let {
@@ -32,68 +36,137 @@ class Game(fileName: String) {
     private fun inMap(coord: Pair<Int, Int>) =
         map.find { clay -> clay.x.contains(coord.first) && clay.y.contains(coord.second) } != null
 
-    fun round(): Int {
-        val newPoints = mutableSetOf<Pair<Int, Int>>()
-        waterPoints.forEach { point ->
+    fun round(danglers: List<Pair<Int, Int>>): List<Pair<Int, Int>> {
+        var newDanglers = mutableSetOf<Pair<Int, Int>>()
+        danglers.forEach { point ->
+            if (point.second == 60) {
+                val z = 1
+            }
             val below = Pair(point.first, point.second + 1)
-            if (below.second < maxY && !waterPoints.contains(below)) {
+            if (below.second <= maxY) {
                 if (!inMap(below)) {
-                    newPoints.add(below);
+                    if (!alltimeDanglers.contains(below)) {
+                        alltimeDanglers.add(below)
+                        newDanglers.add(below)
+                        waterPoints.add(below)
+                    }
                 } else {
                     var fillCandidate = point
                     var rising = false
-                    while (fillLeft(fillCandidate, newPoints, rising) && fillRight(fillCandidate, newPoints, rising)) {
-                        val above = Pair(point.first, point.second - 1)
-                        if (waterPoints.contains(above)) {
-                            fillCandidate = above
-                            rising = true
-                        } else {
-                            break
+                    var notFinished = true
+                    while (notFinished) {
+                        if (fillCandidate.second == 55) {
+                            val z = 1
                         }
+                        val allPointsFilled = mutableListOf<Pair<Int, Int>>(fillCandidate)
+                        listOf (
+                            fillLeft(fillCandidate, rising),
+                            fillRight(fillCandidate, rising)
+                        ).forEach {
+                            val potentialDangler = it.second
+                            if (potentialDangler != null) {
+                                if (!alltimeDanglers.contains(potentialDangler)) {
+                                    alltimeDanglers.add(potentialDangler)
+                                    newDanglers.add(potentialDangler)
+                                    waterPoints.add(potentialDangler)
+                                    notFinished = false
+                                }
+                            }
+                            allPointsFilled.addAll(it.first)
+                        }
+                        if (notFinished) {
+                            val above = allPointsFilled.find {
+                                val above = Pair(it.first, it.second - 1)
+                                alltimeDanglers.contains(it)
+                            }?.copy(second = fillCandidate.second - 1)
+                            if (above != null) {
+                                fillCandidate = above
+                                rising = true
+                            } else {
+                                notFinished = false
+                            }
+                        }
+                        else {
+                            notFinished = false
+                        }
+
                     }
                 }
             }
         }
-        newPoints.removeIf { waterPoints.contains(it) }
-        waterPoints.addAll(newPoints)
-        return newPoints.size
+        return newDanglers.toList()
+    }
+
+    fun draw(yRange: IntRange = (0 .. maxY)) {
+        yRange.forEach {y ->
+            ((minX - 2) .. (maxX + 2)).forEach {x ->
+                val coord = Pair(x, y)
+                print(
+                    when {
+                        inMap(coord) -> '#'
+                        waterPoints.contains(coord) -> '~'
+                        else -> '.'
+                    }
+                )
+            }
+            println()
+
+        }
     }
 
     private fun fillLeft(
         point: Pair<Int, Int>,
-        newPoints: MutableSet<Pair<Int, Int>>,
         rising: Boolean = false,
         delta: Int = -1
-    ): Boolean {
+    ): Pair<List<Pair<Int, Int>>, Pair<Int, Int>?> {
+        val addedPoints = mutableListOf<Pair<Int, Int>>()
         var nextPoint = Pair(point.first + delta, point.second)
         var nextPointBelow = Pair(nextPoint.first, nextPoint.second + 1)
-        while ((inMap(nextPointBelow) || (rising && newPoints.contains(nextPointBelow))) && !inMap(nextPoint)) {
-            newPoints.add(nextPoint)
+        while ((inMap(nextPointBelow) || (rising && waterPoints.contains(nextPointBelow))) && !inMap(nextPoint)) {
+            waterPoints.add(nextPoint)
+            addedPoints.add(nextPoint)
             nextPoint = Pair(nextPoint.first + delta, point.second)
             nextPointBelow = Pair(nextPoint.first, nextPoint.second + 1)
         }
-        return inMap(nextPoint)
+        return Pair(addedPoints, when {
+            inMap(nextPoint) -> null
+            else -> nextPoint // we haven't stopped because we hit a wall, we stopped because we're about to overflow
+        })
     }
 
     private fun fillRight(
         point: Pair<Int, Int>,
-        newPoints: MutableSet<Pair<Int, Int>>,
         rising: Boolean = false
-    ): Boolean {
-        return fillLeft(point, newPoints, rising, 1)
+    ): Pair<List<Pair<Int, Int>>, Pair<Int, Int>?> {
+
+        return fillLeft(point, rising, 1)
     }
 }
 
 fun main(vararg args: String) {
 
-    val game = Game("testInput.txt")
+    val game = Game("input.txt")
 
     println(game.map)
     println("Max Y ${game.maxY}")
 
     var round = 0
-    while (game.round() > 0) {
+    var danglers = listOf(Pair(500, 0))
+    while (danglers.isNotEmpty()) {
+        if (round == 89) {
+            val z = 1
+        }
+        danglers = game.round(danglers)
         round++
-        println("Round $round water ${game.waterPoints.size} -> ${game.waterPoints}")
+        println("Round $round water ${game.waterPoints.size} danglers ${danglers.size}") // -> ${game.waterPoints}
+
+        /*if (round > 85) {
+            game.draw((46..61))
+        }*/
+
     }
+    game.draw()
+
+
+    println("FINAL RESULT ${game.waterPoints.size}")
 }
