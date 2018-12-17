7708cb7ceee1df0c151b16351ca863a30fb9a321
fixedgigha <emissions@cowspassage.net>
2018-12-17 07:41:59 +0000

    Day17

--- src/main/kotlin/advent2018/day17/Day17.kt
+++ src/main/kotlin/advent2018/day17/Day17.kt
@@ -1,11 +1,31 @@
 package advent2018.day17
 
+import java.io.File
 
-fun loadData(fileName: String) {
+data class Clay(val x: IntRange, val y: IntRange)
 
-}
+fun makeRange(input: String): IntRange =
+    Regex("(\\d+)\\.\\.(\\d+)").matchEntire(input)?.let {
+        IntRange(it.groupValues[1].toInt(), it.groupValues[2].toInt())
+    }?: IntRange(input.toInt(), input.toInt())
+
+
+fun loadData(fileName: String) =
+    File("src/main/resources/day17/$fileName").readLines().map{line ->
+        Regex("(x|y)=(\\d+|\\d+\\.\\.\\d+)").findAll(line)
+            .map { match -> match.groupValues[1] to makeRange(match.groupValues[2]) }
+            .groupBy { it.first }
+            .let {map ->
+                Clay(
+                    map.getOrDefault("x", emptyList()).first().second,
+                    map.getOrDefault("y", emptyList()).first().second
+                )
+            }
+    }
+
 
 fun main(vararg args: String) {
-
+    val map = loadData("testInput.txt")
+    println(map)
 
 }
