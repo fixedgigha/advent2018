b48ba1fdc6408bc31fb1c88158f754cad183e3ed
James Speakman <james.speakman@bjss.com>
2018-12-20 17:45:26 +0000

    Day20

--- src/main/kotlin/advent2018/day202/Day20.kt
+++ src/main/kotlin/advent2018/day202/Day20.kt
@@ -6,8 +6,9 @@ val text = File("src/main/resources/day20/input.txt").readText()
 var current = 1
 val coords = mutableSetOf(Triple(0, 0, '0'))
 
-val ANSI_RED = "\u001B[31m"
-val ANSI_RESET = "\u001B[0m"
+const val ANSI_RED = "\u001B[31m"
+const val ANSI_RESET = "\u001B[0m"
+const val ANSI_BLUE = "\u001B[34m"
 
 fun score(startLoc: Triple<Int, Int, Char>): List<Triple<Int, Int, Char>> {
     val branchScores = mutableListOf<MutableList<Triple<Int, Int, Char>>>(mutableListOf())
@@ -60,61 +61,64 @@ fun score(startLoc: Triple<Int, Int, Char>): List<Triple<Int, Int, Char>> {
 fun draw(input: Map<Pair<Int, Int>, Char>, route: List<Pair<Int, Int>>) {
     val routeSet = route.toSet()
 
-    val minX = input.keys.fold(Int.MAX_VALUE) {min, point -> Math.min(min, point.first)} - 1
-    val minY = input.keys.fold(Int.MAX_VALUE) {min, point -> Math.min(min, point.second)} - 1
-    val maxX = input.keys.fold(0) {max, point -> Math.max(max, point.first)} + 1
-    val maxY = input.keys.fold(0) {max, point -> Math.max(max, point.second)} + 1
+    val minX = input.keys.fold(Int.MAX_VALUE) { min, point -> Math.min(min, point.first) } - 1
+    val minY = input.keys.fold(Int.MAX_VALUE) { min, point -> Math.min(min, point.second) } - 1
+    val maxX = input.keys.fold(0) { max, point -> Math.max(max, point.first) } + 1
+    val maxY = input.keys.fold(0) { max, point -> Math.max(max, point.second) } + 1
 
-    (minY..maxY).forEach {y ->
-        (minX..maxX).forEach {x ->
-            val myPoint = Pair(x, y)
-            print(
-                if (routeSet.contains(myPoint)) {
-                    "$ANSI_RED~$ANSI_RESET"
-                }
-                else if (input.contains(myPoint)) {
-                    (input[myPoint]?:'?').toString()
-                }
-                else {
-                    '#'.toString()
-                }
-            )
+    File("../day20.out").printWriter().let { pr ->
+        (minY..maxY).forEach { y ->
+            (minX..maxX).forEach { x ->
+                val myPoint = Pair(x, y)
+                val routeChar = input[myPoint]
+                pr.print(
+                    if (routeSet.contains(myPoint)) {
+                        if ('X' == routeChar)
+                            "${ANSI_BLUE}X$ANSI_RESET"
+                        else
+                            "$ANSI_RED~$ANSI_RESET"
+                    } else if (routeChar != null) {
+                        routeChar.toString()
+                    } else {
+                        '#'.toString()
+                    }
+                )
+            }
+            pr.println()
         }
-        println()
     }
 }
 
 val steps = setOf('0', '.', 'X')
 
 fun candidatesFrom(course: Map<Pair<Int, Int>, Char>,
-                   point: Pair<Int, Int>,
-                   routeSoFar: Set<Pair<Int, Int>>): List<Pair<Int, Int>> {
+                   point: Pair<Int, Int>): List<Pair<Int, Int>> {
     val candidates = mutableListOf<Pair<Int, Int>>()
     val pointWest = point.copy(first = point.first - 2)
     val doorWest  = point.copy(first = point.first - 1)
     val moveWest  = course[pointWest]
-    if (moveWest != null && (!routeSoFar.contains(pointWest)) && steps.contains(moveWest) &&
+    if (moveWest != null && steps.contains(moveWest) &&
             course[doorWest]?:'~' == '|') {
         candidates.add(pointWest)
     }
     val pointEast = point.copy(first = point.first + 2)
     val doorEast  = point.copy(first = point.first + 1)
     val moveEast  = course[pointEast]
-    if (moveEast != null && (!routeSoFar.contains(pointEast)) && steps.contains(moveEast) &&
+    if (moveEast != null &&  steps.contains(moveEast) &&
         course[doorEast]?:'~' == '|') {
         candidates.add(pointEast)
     }
     val pointNorth = point.copy(second = point.second - 2)
     val doorNorth  = point.copy(second = point.second - 1)
     val moveNorth  = course[pointNorth]
-    if (moveNorth != null && (!routeSoFar.contains(pointNorth)) && steps.contains(moveNorth) &&
+    if (moveNorth != null &&  steps.contains(moveNorth) &&
         course[doorNorth]?:'~' == '|') {
         candidates.add(pointNorth)
     }
     val pointSouth = point.copy(second = point.second + 2)
     val doorSouth  = point.copy(second = point.second + 1)
     val moveSouth  = course[pointSouth]
-    if (moveSouth != null && (!routeSoFar.contains(pointSouth)) && steps.contains(moveSouth) &&
+    if (moveSouth != null && steps.contains(moveSouth) &&
         course[doorSouth]?:'~' == '|') {
         candidates.add(pointSouth)
     }
@@ -130,20 +134,28 @@ fun route(course: Map<Pair<Int, Int>, Char>,
     soFar.addAll(routeSoFar)
     var current = start
     while (true) {
-        val candidates = candidatesFrom(course, current, soFar)
-        when (candidates.size) {
+        val candidates = candidatesFrom(course, current)
+        if (candidates.isEmpty()) {
+            // We've found a terminal
+            return listOf(myRoute)
+        }
+        val newCandidates = candidates.filterNot { soFar.contains(it) }
+        when (newCandidates.size) {
             1 -> {
                 current = candidates.first()
                 myRoute.add(current)
                 soFar.add(current)
             }
             0-> {
-                return listOf(myRoute)
+                // trapped in our own loop - deadend
+                return emptyList()
             }
             else -> {
-                return candidates.flatMap {can ->
+                return newCandidates.flatMap {can ->
                     route(course, can, soFar)
-                }.map { newRoute ->
+                }
+                .filter { it.isNotEmpty() }
+                .map { newRoute ->
                     val superRoute = mutableListOf<Pair<Int, Int>>()
                     superRoute.addAll(myRoute)
                     superRoute.addAll(newRoute)
@@ -169,19 +181,22 @@ fun main(vararg args: String) {
 
     val routeGroups = routes.groupBy {it.last()}
     println("Route groups ${routeGroups.size}")
-    val guesses = routeGroups.values.map { it.sortedBy { it.size } }.filter {
-        it.first().size > 1001
-    }
-    val longestRoute = guesses.fold(Pair(0, 0)) { (max, min), next ->
-        val innerMax = next.fold(max) { inMax, inNext ->
-            Math.max(inMax, inNext.size)
+    if (routeGroups.size > 0) {
+        val guesses = routeGroups.values.map { it.sortedBy { it.size } }.filter {
+            it.first().size > 1001
         }
-        if (innerMax > max) {
-            Pair(innerMax, next.first().size)
+        val longestRoute = guesses.fold(Pair(0, 0)) { (max, min), next ->
+            val innerMax = next.fold(max) { inMax, inNext ->
+                Math.max(inMax, inNext.size)
+            }
+            if (innerMax > max) {
+                Pair(innerMax, next.first().size)
+            } else {
+                Pair(max, min)
+            }
         }
-        else {  Pair(max, min) }
-    }
 
-    //draw(course, guesses.first().first())
-    println("My guess ${guesses.size}  longest route $longestRoute")
+        draw(course, guesses.first().first())
+        println("My guess ${guesses.size}  longest route $longestRoute")
+    }
 }
