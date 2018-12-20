0e9bf5004582c7a4960a334029bfe60619ec4572
fixedgigha <emissions@cowspassage.net>
2018-12-20 06:19:14 +0000

    Day20

--- src/main/kotlin/advent2018/day20/Day20.kt
+++ src/main/kotlin/advent2018/day20/Day20.kt
@@ -1,8 +1,41 @@
 package advent2018.day20
 
+import java.io.File
 
+val text = File("src/main/resources/day20/input.txt").readText()
+var current = 1
+
+fun score(): Int {
+    val branchScores = mutableListOf(0)
+    var currentBranch = 0
+    var finished = false
+    while (!finished) {
+        print(text[current])
+        when(text[current]) {
+            '$', ')' -> {
+                current++
+                finished = true
+             }
+            '|' -> {
+                current++
+                currentBranch++
+                branchScores.add(0)
+            }
+            '(' -> {
+                current++
+                branchScores[currentBranch] = (branchScores[currentBranch] + score())
+            }
+            else -> {
+                current++
+                branchScores[currentBranch] = (branchScores[currentBranch] + 1)
+            }
+        }
+    }
+    println()
+    return branchScores.sortedByDescending { it }.first()
+}
 
 fun main(vararg args: String) {
-
-
+    val score = score()
+    println("Final score $score")
 }
