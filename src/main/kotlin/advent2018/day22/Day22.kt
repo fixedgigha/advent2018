9f528b371d0c38c3e5b244b8a8a4ca46efd97a90
fixedgigha <emissions@cowspassage.net>
2018-12-22 21:04:33 +0000

    Day22-Fail

--- src/main/kotlin/advent2018/day22/Day22.kt
+++ src/main/kotlin/advent2018/day22/Day22.kt
@@ -2,9 +2,13 @@ package advent2018.day22
 
 import kotlin.math.absoluteValue
 
-val target = Pair(10, 10)
-const val depth = 510
-var currentFastest = Int.MAX_VALUE
+val target = Pair(7,770)//Pair(10, 10)//
+const val depth = 10647//510//
+var currentFastest = 1067
+const val padFactor = 50
+
+
+val bestPointScores = mutableMapOf<Pair<Pair<Int,Int>, Char>, Int>()
 
 fun moves(point: Pair<Int, Int>): List<Pair<Int, Int>> {
     val result = mutableListOf<Pair<Int, Int>>()
@@ -13,7 +17,7 @@ fun moves(point: Pair<Int, Int>): List<Pair<Int, Int>> {
     result.add(point.copy(second = point.second - 1))
     result.add(point.copy(second = point.second + 1))
 
-    return result.filter { (x, y) -> x >= 0 && y >= 0 && x <= target.first + 8 && y <= target.second + 8}
+    return result.filter { (x, y) -> x >= 0 && y >= 0 && x <= target.first + 100 && y <= target.second + 20}
 }
 
 fun goodMatch(tool: Char): List<Int> =
@@ -34,6 +38,9 @@ fun toolsForMove(point: Pair<Int, Int>) =
         else -> listOf('t', 'n')
     }
 
+val liveRoutes = mutableSetOf(Route())
+val completeRoutes = mutableListOf<Route>()
+
 data class Route(
     var current: Pair<Int, Int> = Pair(0, 0),
     var score: Int = 0,
@@ -41,65 +48,85 @@ data class Route(
     val soFar: MutableSet<Pair<Int, Int>> = mutableSetOf()) {
 
     fun route(): Route {
-        val easyRoutes = mutableListOf<Route>()
-        val hardRoutes = easyRoutes
         var notFinished = true
-        var deadEnd = false
+        var hitTarget = false
         while(notFinished && score <= currentFastest) {
-            if (current == Pair(4, 2)) {
+            if (current == Pair(4, 1) || current == Pair(4, 2)) {
                 val z = 1
                 z
             }
             val moves = moves(current).filterNot{ soFar.contains(it) }.sortedBy { toTarget(it) }
             when {
                 moves.isEmpty() -> {
-                    deadEnd = true
                     notFinished = false
                 }
                 moves.contains(target) -> {
                     score += (if (tool == 't') 1 else 8)
                     notFinished = false
+                    hitTarget = true
                     if (score < currentFastest) {
                         currentFastest = score
+                        completeRoutes.add(this)
                     }
                 }
                 else -> {
-                    soFar.add(current)
-                    val goodMatches = goodMatch(tool)
-                    val easyMoves = moves.filter { goodMatches.contains(cellScore(it)) }
-                    when {
-                        easyMoves.isNotEmpty() -> {
-                            println("Easy move from $current to ${easyMoves.first()} with ${this.tool} also ${easyMoves.size - 1}")
+                    val myScoreKey = Pair(current, tool)
+                    val bestPointScore = bestPointScores.getOrDefault(myScoreKey, Int.MAX_VALUE)
+                    if (score < bestPointScore) {
+                        bestPointScores[myScoreKey] =  score
+                        soFar.add(current)
+                        val goodMatches = goodMatch(tool)
+                        val easyMoves = moves.filter { goodMatches.contains(cellScore(it)) }
+                        val easyScore = score + 1
+                        val hardScore = score + 8
+                        if (easyMoves.isNotEmpty()) {
                             current = easyMoves.first()
-                            score += 1
-                            easyRoutes.addAll(easyMoves.drop(1).map { move ->
-                                copy(current = move)
+                            score = easyScore
+                            liveRoutes.addAll(easyMoves.drop(1).map { move ->
+                                copy(current = move, soFar = cloneRoute())
+                            })
+                        } else {
+                            notFinished = false
+                        }
+                        var harderMoves = moves.filterNot {
+                            easyMoves.contains(it)
+                        }
+                        if (harderMoves.isNotEmpty()) {
+                            val toolsForCurrent = toolsForMove(current)
+                            val toolsForMove = toolsForMove(harderMoves.first())
+                            val tool = toolsForMove.find { toolsForCurrent.contains(it) }?:'?'
+                            if (easyMoves.isEmpty()) {
+                                current = harderMoves.first()
+                                score = hardScore
+                                liveRoutes.add(this)
+                                liveRoutes.add(copy(tool = tool, soFar = cloneRoute()))
+                                harderMoves = harderMoves.drop(1)
+                            }
+                            liveRoutes.addAll(harderMoves.map { move ->
+                                copy(current = move, tool = tool, score = hardScore, soFar = cloneRoute())
                             })
                         }
-                        else -> {
-                            this.current = moves.first()
-                            this.score += 8
-                            val toolsFor1 = toolsForMove(moves.first())
-                            this.tool = toolsFor1.first()
-                            hardRoutes.add(copy(tool = toolsFor1.last()))
-                            hardRoutes.addAll(moves.drop(1).flatMap {move ->
-                                toolsForMove(move).map {tool ->
-                                    copy(current = move, tool = tool)
-                                }
-                            })
-
-                        }
+                    }
+                    else {
+                        notFinished = false
                     }
                 }
             }
         }
-        if (!deadEnd && score <= currentFastest) {
+
+        return if (hitTarget) {
             println("Good route $this")
+            this
         }
-        val routeOptions = mutableListOf<Route>()
-        routeOptions.add(if (deadEnd) deadRoute else this)
-        routeOptions.addAll(easyRoutes.reversed().map { it.route()?: deadRoute})
-        return routeOptions.sortedBy { it.score }.elementAtOrElse(0) {deadRoute}
+        else {
+            deadRoute
+        }
+    }
+
+    private fun cloneRoute(): MutableSet<Pair<Int, Int>> {
+        val newRoute = mutableSetOf<Pair<Int, Int>>()
+        newRoute.addAll(soFar)
+        return newRoute
     }
 }
 
@@ -136,25 +163,36 @@ fun printC(cellScore: Int) =
           else -> '?'
         }
 
+fun mergeRoutes() {
+    liveRoutes.groupBy { Pair(it.current, it.tool) }.filterValues { it.size > 1 }.forEach { (point, routes) ->
+        liveRoutes.removeAll(routes)
+        val superSoFar = mutableSetOf<Pair<Int, Int>>()
+        routes.forEach { superSoFar.addAll(it.soFar) }
+        val superScore = routes.sortedBy { it.score }.first()
+        liveRoutes.add(superScore.copy(soFar = superSoFar))
+    }
+
+}
+
 fun main(vararg args: String) {
-    (0..target.first + 8).forEach { x -> eroLevel(Pair(x, 0)) }
-    (0..target.second + 8).forEach { y -> eroLevel(Pair(0, y)) }
-    (1..target.second + 8).forEach { y ->
-        (1..target.first + 8).forEach { x ->
+
+    (0..target.first + 100).forEach { x -> eroLevel(Pair(x, 0)) }
+    (0..target.second + 20).forEach { y -> eroLevel(Pair(0, y)) }
+    (1..target.second + 20).forEach { y ->
+        (1..target.first + 100).forEach { x ->
             eroLevel(Pair(x, y))
         }
     }
     var riskLevel = 0
-    (0..target.second + 8).forEach { y ->
-        (0..target.first + 8).forEach { x ->
+    (0..target.second + 20).forEach { y ->
+        (0..target.first + 100).forEach { x ->
             val point = Pair(x, y)
             val cellScore = cellScore(point)
-            riskLevel += cellScore
-            print(if (point == target) 'T' else printC(cellScore))
+            val route = winner.soFar.contains(point)
+            if (y <= target.second && x <= target.first) riskLevel += cellScore
+            print(if (point == target) 'T' else if (route) '~' else printC(cellScore))
         }
         println()
     }
     println("Risk level $riskLevel")
-    val result = Route().route()
-    println("Result ${result?.score?:-1}")
 }
