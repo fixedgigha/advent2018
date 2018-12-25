d518d8f518ee1b92f91c1d18c28872659f93ca21
fixedgigha <emissions@cowspassage.net>
2018-12-25 17:49:33 +0000

    Day25

--- src/main/kotlin/advent2018/day25/Day25.kt
+++ src/main/kotlin/advent2018/day25/Day25.kt
@@ -4,8 +4,8 @@ import java.io.File
 import kotlin.math.absoluteValue
 
 
-data class Coord(val x: Int, val y: Int, val z: Int, val t: Int, var inConstellation: Boolean = false) {
-
+data class Coord(val x: Int, val y: Int, val z: Int, val t: Int) {
+    var inConstellation: Boolean = false
     var constellation = mutableSetOf<Coord>()
     fun distance(other: Coord) = distance(this, other)
 }
@@ -35,14 +35,12 @@ data class Game(val fileName:  String) {
     }
 
     fun round2() {
-        val processedCoords = mutableSetOf<Coord>()
-        allCoords.forEach { coord ->
-            if (!processedCoords.contains(coord)) {
-                coord.constellation.addAll(coord.constellation.flatMap { it.constellation })
-                coord.constellation.filterNot { it == coord } .forEach { it.constellation.clear() }
-                processedCoords.add(coord)
-                processedCoords.addAll(coord.constellation)
+        while (true) {
+            var changed = false
+            allCoords.forEach { coord ->
+                changed = changed || coord.constellation.addAll(coord.constellation.flatMap { it.constellation })
             }
+            if (!changed) break
         }
     }
 }
@@ -56,11 +54,11 @@ fun distance(from: Coord, to: Coord): Int =
 
 
 fun main(vararg args: String) {
-    val game = Game("src/main/resources/day25/testInput2.txt")
+    val game = Game("src/main/resources/day25/input.txt")
     println("Number of points ${game.allCoords.size}")
     game.round1()
     game.round2()
-    val mutipleConstellatios = game.allCoords.filter { it.constellation.isNotEmpty() }
+    val mutipleConstellatios = game.allCoords.map { it.constellation }.filter { it.isNotEmpty() }.toSet()
     val singletons = game.allCoords.filterNot { it.inConstellation }
 
     println("Result - multis ${mutipleConstellatios.size} singles ${singletons.size} - TOTAL - ${mutipleConstellatios.size + singletons.size}")
