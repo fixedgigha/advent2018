c2f09c2858ee6084173b9a2f4a9b8db0f487f72d
fixedgigha <emissions@cowspassage.net>
2018-12-26 10:42:32 +0000

    Day25 massively faster

--- src/main/kotlin/advent2018/day25/Day25.kt
+++ src/main/kotlin/advent2018/day25/Day25.kt
@@ -17,13 +17,16 @@ data class Game(val fileName:  String) {
     init {
         allCoords.addAll(File(fileName).readLines().map { line ->
             val points = line.split(",")
-            Coord(points[0].toInt(),points[1].toInt(), points[2].toInt(), points[3].toInt())
+            Coord(points[0].toInt(), points[1].toInt(), points[2].toInt(), points[3].toInt())
         })
     }
 
-    fun round1() {
+    /**
+     * For each point find all *in distance* other points
+     */
+    fun findNeighbours() {
         allCoords.forEach { coord ->
-            allCoords.filterNot { it == coord}.forEach { target ->
+            allCoords.filterNot { it == coord }.forEach { target ->
                 if (coord.distance(target) <= 3) {
                     coord.constellation.add(target)
                     target.constellation.add(coord)
@@ -34,13 +37,31 @@ data class Game(val fileName:  String) {
         }
     }
 
-    fun round2() {
-        while (true) {
-            var changed = false
-            allCoords.forEach { coord ->
-                changed = changed || coord.constellation.addAll(coord.constellation.flatMap { it.constellation })
+    private fun collectChildren(from: Coord, into: MutableSet<Coord>) {
+        val additions = from.constellation.filterNot { into.contains(it) }
+        if (additions.isNotEmpty()) {
+            into.addAll(additions)
+            additions.forEach { collectChildren(it, into) }
+        }
+
+    }
+
+    val multiConstellations = mutableSetOf<Set<Coord>>()
+
+    /**
+     * Merge > 1 constellations from round 1 into full constellations
+     */
+    fun mergeConstellations() {
+        val alreadyProcessed = mutableListOf<Coord>()
+        allCoords.forEach { coord ->
+            if (coord.inConstellation && !alreadyProcessed.contains(coord)) {
+                alreadyProcessed.add(coord)
+                val iterSet = mutableSetOf<Coord>()
+                iterSet.addAll(coord.constellation)
+                iterSet.forEach {  collectChildren(it, coord.constellation) }
+                alreadyProcessed.addAll(coord.constellation)
+                multiConstellations.add(coord.constellation)
             }
-            if (!changed) break
         }
     }
 }
@@ -56,9 +77,9 @@ fun distance(from: Coord, to: Coord): Int =
 fun main(vararg args: String) {
     val game = Game("src/main/resources/day25/input.txt")
     println("Number of points ${game.allCoords.size}")
-    game.round1()
-    game.round2()
-    val mutipleConstellatios = game.allCoords.map { it.constellation }.filter { it.isNotEmpty() }.toSet()
+    game.findNeighbours()
+    game.mergeConstellations()
+    val mutipleConstellatios = game.multiConstellations
     val singletons = game.allCoords.filterNot { it.inConstellation }
 
     println("Result - multis ${mutipleConstellatios.size} singles ${singletons.size} - TOTAL - ${mutipleConstellatios.size + singletons.size}")
